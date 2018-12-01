dd3dcbe40d1145a5a006ebdf4cf700866ac10fc0
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2018-12-01 18:44:10 -0200

    add ssr from mongodb

--- server/controller/MainController.js
+++ server/controller/MainController.js
@@ -1,10 +1,18 @@
 // cara burro que recebe um pedido e responde
 // pergunta pro model e responde pra view
 
+const service = require('../service/MovieService')
+
 //objeto comeca com letra maiuscula
 const MainController = {
     home(request, response, next) {
-        response.render('home', {serverRender: 'Novatec 2018'});
+        service.list({})
+            .then(datas => {
+                response.render('home', {
+                    title: 'Server-side Rendering',
+                    items: datas[0]
+                })
+            })
     },
     contact(request, response, next) {
         response.send('Página de contato');
