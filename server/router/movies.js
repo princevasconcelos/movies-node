dfe4412e46d4ed6f172b8afc2a84c1e72c5ed9f8
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2018-12-01 12:08:01 -0200

    add isValidId middleware for movies

--- server/router/movies.js
+++ server/router/movies.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const controller = require('../controller/MovieController');
 
+//nem chama o controler se nao passar por esse middleware
+function isValidId(request, response, next) {
+    if (request.params.id && request.params.id.length !== 24) {
+        let err = new Error('invalid id')
+        err.status = 422
+        return next(err)
+    }
+    next()
+}
+
 router.get('/', controller.list);
-router.get('/:id', controller.byId);
+router.get('/:id', isValidId, controller.byId);
 // router.post('/', controller.create);
 // router.put('/', controller.update);
 // router.delete('/', controller.delete);
